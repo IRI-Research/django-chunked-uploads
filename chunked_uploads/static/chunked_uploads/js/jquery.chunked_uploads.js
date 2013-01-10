efb97544631cc07132a38ed7601f0c15ea639a16
JulienR2 <Julien.rougeron@gmail.com>
2013-01-10 16:19:36 +0100

    clean unused js and mooved main.js into jquery.chunked_uploads.js

--- chunked_uploads/static/chunked_uploads/js/jquery.chunked_uploads.js
+++ chunked_uploads/static/chunked_uploads/js/jquery.chunked_uploads.js
@@ -1,39 +1,114 @@
-jQuery(function($) {
-
-    var ChunkedUpload = function(form, options) {
-        this.options = $.extend({}, $.fn.phileo.defaults, options);
-        this.$form = $(form);
-
-        this.$count = $(this.options.count);
-
-        var self = this;
-        this.$form.submit(function(event) {
-            event.preventDefault();
-
-            $.ajax({
-                url: self.$form.attr('action'),
-                type: "POST",
-                data: self.$form.serialize(),
-                statusCode: {
-                    200: function(data, textStatus, jqXHR) {
-                        self.$count.text(data.likes_count);
-                        self.$form[data.liked ? 'addClass' : 'removeClass'](self.options.toggle_class);
-                    }
-                }
-            });
+$(function () {
+	'use strict';
+	var is_canceled=false;
+	var is_paused=false;
+	var data_resume = null;
+	$('#start_upload').attr('disabled', true);
+	$('#pause_upload').attr('disabled', true);
+	$('#resume_upload').attr('disabled', true);
+	$('#cancel_upload').attr('disabled', true);
+	
+	$('#fileupload').fileupload({        
+        url: chunked_uploads_endpoints.upload_url,
+	   	dataType: 'json',
+	   	acceptFileTypes: /(\.|\/)(avi|mpe?g|wmv)$/i,
+	   	maxChunkSize: 1048576,
+	   	maxNumberOfFiles: 1,
+	    multipart: false,
+	    autoUpload: false,
+	    xhrFields: {
+	        withCredentials: true
+	    },
+	    add: function (e, data) {
+	    	$('#start_upload').attr('disabled', false);
+	    	
+	    	$('#start_upload').one('click', function (e) {
+	            e.preventDefault();
+	            chunked_uploads_endpoints.jqXHR = data.submit();
+	        });
+	    },
+	    done: function (e, data) {
+	    	chunked_uploads_endpoints.done_url = chunked_uploads_endpoints.done_url.replace('00000000-0000-0000-0000-000000000000', data.result[0].upload_uuid);
+	    	$.getJSON(chunked_uploads_endpoints.done_url, function(upload) {
+	    		if (typeof chunked_uploads_video_url === "function") {
+	    			chunked_uploads_video_url(upload[0].video_url);
+	    		}
+	    	});
+	        $('#start_upload').hide();
+    		$('#pause_upload').hide();
+    		$('#resume_upload').hide();
+    		$('#cancel_upload').hide();
+    		
+	    },
+	    progressall: function (e, data) {
+	    	var progress = parseInt(data.loaded / data.total * 100, 10);
+	        $('.progress').css(
+	            'width',
+	            progress + '%'
+	        );
+    	},
+	    send: function(e, data) {
+	    	$("#fileupload").hide();
+	    	$('#start_upload').hide();
+            $('#pause_upload').attr('disabled', false);
+        	$('#cancel_upload').attr('disabled', false);
+	    },
+	    fail: function(e, data) {
+	    	data_resume = data;
+	    	if (typeof chunked_uploads_error === "function") {
+    			chunked_uploads_error(true);
+    		}
+	    }
+    });
+	   
+    $('#cancel_upload').click(function (e) {
+		if (!is_paused){
+			is_canceled = true;
+			chunked_uploads_endpoints.jqXHR.abort();
+	    	$('#pause_upload').attr('disabled', true);
+			$('#resume_upload').attr('disabled', true);
+			setTimeout(function() {
+				$.getJSON(chunked_uploads_endpoints.upload_url, function(current_upload) {	
+					$.ajax({
+	    	    		type: current_upload[0].delete_type,
+	    	    		url: current_upload[0].delete_url
+	    		    });
+	        	});
+			},1000);
+		}
+		else{
+			$.getJSON(chunked_uploads_endpoints.upload_url, function(current_upload) {	
+    			$.ajax({
+    	    		type: current_upload[0].delete_type,
+    	    		url: current_upload[0].delete_url
+    		    });
+        	});
+		}
+	});
+	
+	$('#pause_upload').click(function (e) {
+		is_paused = true;
+		chunked_uploads_endpoints.jqXHR.abort();
+		$('#pause_upload').attr('disabled', true);
+		$('#resume_upload').attr('disabled', false);
+	});
+	
+	$('#resume_upload').click(function (e) {
+		is_paused = false;
+        $.getJSON(chunked_uploads_endpoints.upload_url, function (current_upload) {
+        	data_resume.uploadedBytes = current_upload[0].size;
+        	chunked_uploads_endpoints.jqXHR = data_resume.submit();
         });
-    };
-
-    $.fn.phileo = function(options) {
-        $(this).each(function(i, el) {
-            var chunked_upload = new ChunkedUpload(el, options);
-            $(el).data('DjangoChunkedUpload', {instance: chunked_upload});
-        });
-        return this;
-    };
-
-    $.fn.phileo.defaults = {
-        toggle_class: 'phileo-liked',
-        count: false
-    };
+		$('#resume_upload').attr('disabled', true);
+		$('#pause_upload').attr('disabled', false);
+	});
+	
+	$('#clean_upload').click(function (e) {
+		$.getJSON(chunked_uploads_endpoints.upload_url, function(current_upload) {	
+			$.ajax({
+	    		type: current_upload[0].delete_type,
+	    		url: current_upload[0].delete_url
+		    });
+    	});
+	});
 });
