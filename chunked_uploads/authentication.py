d2f449539dfc717454190b61dd1ecdbb26ff84a5
JulienR2 <Julien.rougeron@gmail.com>
2013-01-31 16:37:10 +0100

    tastypie like authentication

---
+++ chunked_uploads/authentication.py
@@ -0,0 +1,184 @@
+import base64
+import hmac
+import time
+import uuid
+
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext as _
+from django.http import HttpResponse
+
+try:
+    from hashlib import sha1
+except ImportError:
+    import sha
+    sha1 = sha.sha
+
+try:
+    import python_digest
+except ImportError:
+    python_digest = None
+
+try:
+    import oauth2
+except ImportError:
+    oauth2 = None
+
+try:
+    import oauth_provider
+except ImportError:
+    oauth_provider = None
+    
+class HttpUnauthorized(HttpResponse):
+    status_code = 401
+
+class Authentication(object):
+    """
+    A simple base class to establish the protocol for auth.
+
+    By default, this indicates the user is always authenticated.
+    """
+    def is_authenticated(self, request, **kwargs):
+        """
+        Identifies if the user is authenticated to continue or not.
+
+        Should return either ``True`` if allowed, ``False`` if not or an
+        ``HttpResponse`` if you need something custom.
+        """
+        return True
+
+    def get_identifier(self, request):
+        """
+        Provides a unique string identifier for the requestor.
+
+        This implementation returns a combination of IP address and hostname.
+        """
+        return "%s_%s" % (request.META.get('REMOTE_ADDR', 'noaddr'), request.META.get('REMOTE_HOST', 'nohost'))
+
+
+class BasicAuthentication(Authentication):
+    """
+    Handles HTTP Basic auth against a specific auth backend if provided,
+    or against all configured authentication backends using the
+    ``authenticate`` method from ``django.contrib.auth``.
+
+    Optional keyword arguments:
+
+    ``backend``
+        If specified, use a specific ``django.contrib.auth`` backend instead
+        of checking all backends specified in the ``AUTHENTICATION_BACKENDS``
+        setting.
+    ``realm``
+        The realm to use in the ``HttpUnauthorized`` response.  Default:
+        ``django-tastypie``.
+    """
+    def __init__(self, backend=None, realm=''):
+        self.backend = backend
+        self.realm = realm
+
+    def _unauthorized(self):
+        response = HttpUnauthorized()
+        # FIXME: Sanitize realm.
+        response['WWW-Authenticate'] = 'Basic Realm="%s"' % self.realm
+        return response
+
+    def is_authenticated(self, request, **kwargs):
+        """
+        Checks a user's basic auth credentials against the current
+        Django auth backend.
+
+        Should return either ``True`` if allowed, ``False`` if not or an
+        ``HttpResponse`` if you need something custom.
+        """
+        if not request.META.get('HTTP_AUTHORIZATION'):
+            return self._unauthorized()
+
+        try:
+            (auth_type, data) = request.META['HTTP_AUTHORIZATION'].split()
+            if auth_type != 'Basic':
+                return self._unauthorized()
+            user_pass = base64.b64decode(data)
+        except:
+            return self._unauthorized()
+
+        bits = user_pass.split(':', 1)
+
+        if len(bits) != 2:
+            return self._unauthorized()
+
+        if self.backend:
+            user = self.backend.authenticate(username=bits[0], password=bits[1])
+        else:
+            user = authenticate(username=bits[0], password=bits[1])
+
+        if user is None:
+            return self._unauthorized()
+
+        request.user = user
+        return True
+
+    def get_identifier(self, request):
+        """
+        Provides a unique string identifier for the requestor.
+
+        This implementation returns the user's basic auth username.
+        """
+        return request.META.get('REMOTE_USER', 'nouser')
+
+
+class ApiKeyAuthentication(Authentication):
+    """
+    Handles API key auth, in which a user provides a username & API key.
+
+    Uses the ``ApiKey`` model that ships with tastypie. If you wish to use
+    a different model, override the ``get_key`` method to perform the key check
+    as suits your needs.
+    """
+    def _unauthorized(self):
+        return HttpUnauthorized()
+
+    def is_authenticated(self, request, **kwargs):
+        """
+        Finds the user and checks their API key.
+
+        Should return either ``True`` if allowed, ``False`` if not or an
+        ``HttpResponse`` if you need something custom.
+        """
+        from django.contrib.auth.models import User
+
+        username = request.GET.get('username') or request.POST.get('username') or request.META.get('HTTP_USERNAME')
+        api_key = request.GET.get('api_key') or request.POST.get('api_key') or request.META.get('HTTP_API_KEY')
+        
+        if not username or not api_key:
+            return self._unauthorized()
+
+        try:
+            user = User.objects.get(username=username)
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
+            return self._unauthorized()
+
+        request.user = user
+        return self.get_key(user, api_key)
+
+    def get_key(self, user, api_key):
+        """
+        Attempts to find the API key for the user. Uses ``ApiKey`` by default
+        but can be overridden.
+        """
+        from chunked_uploads.models import ApiKey
+
+        try:
+            ApiKey.objects.get(user=user, key=api_key)
+        except ApiKey.DoesNotExist:
+            return self._unauthorized()
+
+        return True
+
+    def get_identifier(self, request):
+        """
+        Provides a unique string identifier for the requestor.
+
+        This implementation returns the user's username.
+        """
+        return request.REQUEST.get('username', 'nouser')
