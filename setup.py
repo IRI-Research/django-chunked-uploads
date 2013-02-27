e43ef1af5ae1270fe79bb26ea06ec04920d7b405
Yves-Marie Haussonne <ymh.work+github@gmail.com>
2013-02-27 17:47:24 +0100

    Add gitignore and adapt setup.py
    Signed-off-by: Yves-Marie Haussonne <ymh.work+github@gmail.com>

--- setup.py
+++ setup.py
@@ -111,27 +111,35 @@ AUTHOR_EMAIL = "paltman@eldarion.com"
 URL = "https://github.com/eldarion/django-chunked-uploads"
 VERSION = __import__(PACKAGE).__version__
 
+def launch_setup(script_name, script_args):
+    setup(
+        script_name = script_name,
+        script_args = script_args,
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=read("README.md"),
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        license="BSD",
+        url=URL,
+        packages=find_packages(exclude=["tests.*", "tests"]),
+        package_data=find_package_data(PACKAGE, only_in_packages=False),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Web Environment",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Framework :: Django",
+        ],
+        zip_safe=False,
+    )
 
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=read("README.md"),
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license="BSD",
-    url=URL,
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    package_data=find_package_data(PACKAGE, only_in_packages=False),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Framework :: Django",
-    ],
-    zip_safe=False,
-)
-
+if __name__ == "__main__":
+    
+    script_name = os.path.basename(sys.argv[0])
+    script_args = sys.argv[1:]
+    
+    launch_setup(script_name, script_args)
